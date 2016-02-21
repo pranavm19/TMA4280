79bdb13089c2bf6a8a70af51e9ae080992383680
Pranav Mamidanna <pranav.mamidanna@gmail.com>
2016-02-21 22:47:23 +0100

    Update pisquared_mpi.c

--- pisquared_mpi.c
+++ pisquared_mpi.c
@@ -5,27 +5,26 @@
 
 int main(int argc, char** argv)
 {
-	double time_start;
 	if (argc < 2) {
 		printf ("Requires argument: k.\n");
 		return 1;
-	}
-
-	int k = atoi(argv[1]);
-	if (k <= 2){
-		printf("Give me a number greater than or equal to 3 :)\n");
-		exit (1);
 	}
 
 	int nprocs, rank ;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+	
+	int k = atoi(argv[1]);
+	if (k <= 2){
+		printf("Give me a number greater than or equal to 3 :)\n");
+		exit (1);
+	}
 	int n = 1 << k;	
 	// A Vector to store the values for v[i] = i^(-2).
 	double* v = malloc(n*sizeof(double));
-
+	double time_start;
+	
 	if (rank == 0) {
 		time_start = MPI_Wtime();	
 		// Set Elements of Vector.
@@ -51,8 +50,9 @@ int main(int argc, char** argv)
 		printf("Execution Time: %0.16f \n", duration);
 	}
 	
-	MPI_Finalize ();
+	
 	free(v);
+	MPI_Finalize ();
 	return 0;
 }
 
